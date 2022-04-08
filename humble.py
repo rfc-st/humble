69cba54d3da2f3218e8ca0da42c19afb601a5906
Rafa Faura <rafael.fcucalon@gmail.com>
2022-04-08 20:12:26 +0200

    Fix output.

--- humble.py
+++ humble.py
@@ -520,7 +520,7 @@ if 'Access-Control-Allow-Origin' in headers:
                 print(" Review the value '" +
                       headers["Access-Control-Allow-Origin"] + "' regarding \
 your CORS (Cross-origin resource sharing) requirements.")
-            print("")
+                print("")
     i_cnt += 1
 
 if 'Allow' in headers:
