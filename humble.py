3c7f73e50bd0bf530b3834b698612fb8485740e3
Rafa Faura <rafael.fcucalon@gmail.com>
2022-05-21 18:18:44 +0200

    Update User Agent sent.

--- humble.py
+++ humble.py
@@ -344,8 +344,8 @@ request_exceptions()
 
 # Headers retrieval
 
-c_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0)\
- Gecko/20100101 Firefox/89.0'}
+c_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; \
+ rv:100.0) Gecko/20100101 Firefox/100.0'}
 
 r = requests.get(domain, headers=c_headers, timeout=60)
 headers = r.headers
