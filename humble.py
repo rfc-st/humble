c3be676819160e5cc668a1f3939ba87570eb535e
Rafa Faura <rafael.fcucalon@gmail.com>
2022-11-01 17:33:41 +0100

    Indication of minimum versions of required dependencies

--- humble.py
+++ humble.py
@@ -56,7 +56,7 @@ if platform.system() == 'Windows':
 else:
     spacing = '\r\n'
 
-version = '\r\n' + "2022/10/31. Rafa 'Bluesman' Faura \
+version = '\r\n' + "2022/11/01. Rafa 'Bluesman' Faura \
 (rafael.fcucalon@gmail.com)" + '\r\n' + '\r\n'
 
 
