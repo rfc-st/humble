b3641e79ea1e3c7378184a7a2c0a1961cada3c3e
Rafa Faura <rafael.fcucalon@gmail.com>
2022-12-12 20:34:41 +0100

    4 new fingerprint headers

--- humble.py
+++ humble.py
@@ -60,7 +60,7 @@ if platform.system() == 'Windows':
 else:
     spacing = '\r\n'
 
-version = '\r\n' + "2022-12-11. Rafa 'Bluesman' Faura \
+version = '\r\n' + "2022-12-12. Rafa 'Bluesman' Faura \
 (rafael.fcucalon@gmail.com)" + '\r\n' + '\r\n'
 
 
