019a22dad1199833c6fb1096888f8618e927765c
Rafa Faura <rafael.fcucalon@gmail.com>
2023-07-09 18:34:48 +0200

    Feature: 6 new fingerprint headers

--- humble.py
+++ humble.py
@@ -66,7 +66,7 @@ URL_S = ' URL  : '
 
 export_date = datetime.now().strftime("%Y%m%d")
 now = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
-version = datetime.strptime('2023-06-30', '%Y-%m-%d').date()
+version = datetime.strptime('2023-07-09', '%Y-%m-%d').date()
 
 
 class PDF(FPDF):
