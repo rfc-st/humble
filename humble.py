9f4fe33fe7975348806916584c6a228de9951b6e
Rafa Faura <rafael.fcucalon@gmail.com>
2024-08-05 17:27:25 +0200

    Dependencies: Bumped 'requests' to >=2.32.3
    Due to errors/warnings related to urllib3/chardet/charset_normalizer

--- humble.py
+++ humble.py
@@ -108,7 +108,7 @@ tps://github.com/rfc-st/humble')
 URL_STRING = ('rfc-st', ' URL  : ', 'caniuse')
 
 current_time = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
-local_version = datetime.strptime('2024-07-26', '%Y-%m-%d').date()
+local_version = datetime.strptime('2024-08-05', '%Y-%m-%d').date()
 
 
 class SSLContextAdapter(requests.adapters.HTTPAdapter):
