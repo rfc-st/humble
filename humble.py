aa321922a225bf37f2935cd61ce2205245ab0d5d
Rafa Faura <rafael.fcucalon@gmail.com>
2024-09-13 17:22:45 +0200

    Feature: 11 new fingerprint headers

--- humble.py
+++ humble.py
@@ -108,7 +108,7 @@ tps://github.com/rfc-st/humble')
 URL_STRING = ('rfc-st', ' URL  : ', 'caniuse')
 
 current_time = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
-local_version = datetime.strptime('2024-09-07', '%Y-%m-%d').date()
+local_version = datetime.strptime('2024-09-13', '%Y-%m-%d').date()
 
 
 class SSLContextAdapter(requests.adapters.HTTPAdapter):
