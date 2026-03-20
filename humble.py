e49aa09440333a548b2da76bf23b7c23dc7e8e8d
Rafa Faura <rafael.fcucalon@gmail.com>
2026-03-20 20:12:31 +0100

    Feature: 19 new fingerprint headers

--- humble.py
+++ humble.py
@@ -180,7 +180,7 @@ XFRAME_CHECK = 'X-Frame-Options ('
 XML_STRING = ('Ref: ', 'Value: ', 'Valor: ')
 
 current_time = datetime.now().strftime("%Y/%m/%d - %H:%M:%S")
-local_version = datetime.strptime('2026-03-14', '%Y-%m-%d').date()
+local_version = datetime.strptime('2026-03-20', '%Y-%m-%d').date()
 
 BANNER_VERSION = f'{URL_LIST[4]} | v.{local_version}'
 
