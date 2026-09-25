24d95ed45a2be5a84b906af74ccafe1a4de59a39
Rafa Faura <rafael.fcucalon@gmail.com>
2026-09-25 17:59:09 +0200

    Minor: Align 'insecure.txt' titles with the reported findings

--- tests/test_humble.py
+++ tests/test_humble.py
@@ -988,7 +988,7 @@ def cleanup_analysis_history():
         fsync(original_file.fileno())
 
 
-local_version = date.fromisoformat("2026-09-19")
+local_version = date.fromisoformat("2026-09-25")
 parser = ArgumentParser(
     formatter_class=lambda prog: RawDescriptionHelpFormatter(
         prog, max_help_position=34,
